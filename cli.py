de5b83909522562da27be3028d1d0deadf4d5cc1
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2018-07-26 03:11:52 +0800

    add oversize scrollable feature

--- cli.py
+++ cli.py
@@ -2,24 +2,28 @@ import curses
 from log.logger import initlogger
 from oxford import oxford
 import utils
+import logging
+import time
 
 
 
 def main_program():
     word = []
     auto_list = []
+    str_list = []
+    cur_position = 0
     key = ''
     senses = ''
 
+    utils_obj = utils.OxfordOutPutUtils(myScreen)
+
     myScreen.erase()
-    myScreen.addstr(1, 1, '> ' + ''.join(word),\
-        curses.color_pair(1) | curses.A_BOLD)
+    myScreen.addstr(1, 1, '> ' + ''.join(word), curses.color_pair(1) | curses.A_BOLD)
 
     while key != 27:
         key = myScreen.getch(1, 3 + len(word))
         myScreen.erase()
-
-        if key == 127 and len(word) != 0:
+        if key == 263 and len(word) != 0:
             #  back space
             word.pop()
         elif key >= 97 and key <= 122 or key >= 65 and key <= 90 or key == 32:
@@ -28,15 +32,26 @@ def main_program():
         elif key == 9:
             # auto complete
             auto_list = oxford.get_auto_word(''.join(word))
-            utils.color_add_autocomplete(auto_list, myScreen)
+            utils_obj.color_add_autocomplete(auto_list)
         elif key >= 49 and key <= 54:
+            # choice word
             choice = int(chr(key))
             if len(auto_list) >= choice:
                 word = list(auto_list[choice - 1])
+        elif key == curses.KEY_DOWN:
+            # scroll able - down
+            cur_position = utils_obj.scroll(
+                str_list, cur_position, curses.LINES)
+        elif key == curses.KEY_UP:
+            # scroll able - up
+            cur_position = utils_obj.scroll(
+                str_list, cur_position, curses.LINES, down=False)
         elif key == 10:
+            cur_position = 0
             # get senses
             senses = oxford.get_word_sense(''.join(word))
-            utils.color_add_senses(senses, myScreen)
+            str_list = senses.split('\n')
+            utils_obj.color_add_senses(str_list)
         myScreen.addstr(1, 1, '> ' + ''.join(word), curses.color_pair(1) | curses.A_BOLD)
         myScreen.refresh()
 
@@ -44,8 +59,11 @@ if __name__ == '__main__':
     # init part
     initlogger()
     myScreen = curses.initscr()
+    myScreen.border(0)
 
     curses.start_color()
+    # curses.noecho()
+    myScreen.keypad(1)
 
     utils.init_color_pair()
     # main_program
--- utils.py
+++ utils.py
@@ -2,9 +2,6 @@ import curses
 import logging
 import re
 
-# keywords
-word_classes = ['Noun', 'Verb', 'Adjective', 'Adverb', 'Pronoun',
-    'Preposition', 'Conjunction', 'Determiner', 'Exclamation', 'Interjection']
 
 def init_color_pair():
     # colors
@@ -13,53 +10,84 @@ def init_color_pair():
     curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
 
+class OxfordOutPutUtils(object):
 
-def color_add_senses(context, screen_obj):
-    str_list = context.split('\n')
-    context_length = len(str_list)
-    try:
-        for i in range(2, context_length + 2):
-            # word classes
-            if str_list[i - 2].strip() in word_classes:
-                screen_obj.addstr(
-                    i + 2, 1,
-                    str_list[i - 2],
-                    curses.color_pair(1) | curses.A_BOLD | curses.A_REVERSE)
-                continue
-            # spelling
-            if str_list[i - 2].strip().startswith('phonetic spelling'):
-                screen_obj.addstr(
-                    i + 2, 1,
-                    str_list[i - 2],
-                    curses.color_pair(2) | curses.A_DIM)
-                continue
-            # short definitions
-            if str_list[i - 2].strip().startswith('short definitions'):
-                screen_obj.addstr(
-                    i + 2, 1, str_list[i - 2],
-                    curses.A_DIM)
-                continue
-            # definitions
-            if str_list[i - 2].strip().startswith('definitions'):
-                screen_obj.addstr(
-                    i + 2, 1, str_list[i - 2],
-                    curses.color_pair(3))
-                continue
-            # index
-            if re.match(r'[0-9]+\.', str_list[i - 2].strip()):
-                screen_obj.addstr(
-                    i + 2, 1, str_list[i - 2],
-                    curses.A_BOLD)
-                continue
-            screen_obj.addstr(i + 2, 1, str_list[i - 2])
-    except Exception as e:
-        logging.error("utils color_add_senses {}".format(e))
+    # keywords
+    word_classes = ['Noun', 'Verb', 'Adjective', 'Adverb', 'Pronoun',
+        'Preposition', 'Conjunction', 'Determiner', 'Exclamation', 'Interjection']
+
+    def __init__(self, screen_obj):
+        self.screen_obj = screen_obj
+        self.position = 0
+
+    def color_add_senses(self, str_list):
+        context_length = len(str_list)
+        try:
+            for i in range(2, context_length + 2):
+                # word classes
+                if str_list[i - 2].strip() in self.word_classes:
+                    self.screen_obj.addstr(
+                        i + 2, 1,
+                        str_list[i - 2],
+                        curses.color_pair(1) | curses.A_BOLD | curses.A_REVERSE)
+                    continue
+                # spelling
+                if str_list[i - 2].strip().startswith('phonetic spelling'):
+                    self.screen_obj.addstr(
+                        i + 2, 1,
+                        str_list[i - 2],
+                        curses.color_pair(2) | curses.A_DIM)
+                    continue
+                # short definitions
+                if str_list[i - 2].strip().startswith('short definitions'):
+                    self.screen_obj.addstr(
+                        i + 2, 1, str_list[i - 2],
+                        curses.A_DIM)
+                    continue
+                # definitions
+                if str_list[i - 2].strip().startswith('definitions'):
+                    self.screen_obj.addstr(
+                        i + 2, 1, str_list[i - 2],
+                        curses.color_pair(3))
+                    continue
+                # index
+                if re.match(r'[0-9]+\.', str_list[i - 2].strip()):
+                    self.screen_obj.addstr(
+                        i + 2, 1, str_list[i - 2],
+                        curses.A_BOLD)
+                    continue
+                self.screen_obj.addstr(i + 2, 1, str_list[i - 2])
+        except Exception as e:
+            logging.error("utils color_add_senses {}".format(e))
 
 
-def color_add_autocomplete(auto_list, screen_obj):
-    position = 0
-    for i, v in enumerate(auto_list):
-        temp_str = '{}.{}'.format(i+1, v)
-        screen_obj.addstr(
-            2, position, temp_str, curses.color_pair(4))
-        position += len(temp_str) + 1
+    def color_add_autocomplete(self, auto_list):
+        position = 0
+        for i, v in enumerate(auto_list):
+            temp_str = '{}.{}'.format(i+1, v)
+            self.screen_obj.addstr(
+                2, position, temp_str, curses.color_pair(4))
+            position += len(temp_str) + 1
+
+    def scroll(self, str_list, cur_position, line_num, down=True):
+        str_list_len = len(str_list)
+        logging.info("str_len %s line_num %s cur_position %s self.position %s" \
+            % (str_list_len, line_num, cur_position, self.position))
+        if line_num >= str_list_len:
+            self.color_add_senses(str_list)
+            return 0
+        if cur_position > str_list_len - 1:
+            # logging.info(str_list[self.position:])
+            self.color_add_senses(str_list[self.position-1:])
+            return self.position - 1
+        if down:
+            self.position = cur_position + 1
+            self.color_add_senses(str_list[self.position:])
+            return self.position
+        if cur_position <= 0:
+            self.color_add_senses(str_list[self.position:])
+            return cur_position
+        # logging.info(str_list[self.position:])
+        self.position = cur_position - 1
+        self.color_add_senses(str_list[self.position:])
+        return self.position
